6dab97eddf8bb4efae8aa3d2bd506e0fc27ba305
Tim Wolfs <tim.wolfs@ara.nl>
2023-10-18 16:27:38 +0200

    styling update

--- src/slices/RichText/index.tsx
+++ src/slices/RichText/index.tsx
@@ -17,7 +17,7 @@ type RichTextProps = SliceComponentProps<Content.RichTextSlice>;
 
 export default function RichText({ slice }: RichTextProps) {
   return (
-    <section className="container">
+    <section className="container py-8 lg:py-20 xl:py-32">
       <div className="grid grid-cols-4 lg:grid-cols-12 gap-x-4 gap-y-8">
         <div className="col-span-4 lg:col-span-6 lg:col-start-4 lg:col-end-10">
           <PrismicRichText field={slice.primary.content} components={components} />
--- src/slices/TextWithImage/index.tsx
+++ src/slices/TextWithImage/index.tsx
@@ -11,10 +11,10 @@ export type TextWithImageProps = SliceComponentProps<Content.TextWithImageSlice>
 
 const TextWithImage = ({ slice }: TextWithImageProps): JSX.Element => {
   return (
-    <section className="section text-image">
-      <div className="grid grid-cols-4 lg:grid-cols-12 gap-x-4 gap-y-8">
-        <div className={cn(slice.primary.text_right ? "lg:order-1":"order-0", "col-span-4 lg:col-span-6")}>
-          <div className="h-full flex flex-col justify-center items-start p-6 lg:p-20 xl:p-32 2xl:p-40">
+    <section>
+      <div className="grid grid-cols-4 lg:grid-cols-12 gap-y-8">
+        <div className={cn(slice.primary.text_right ? "lg:order-1":"order-0 pt-8 lg:pt-0", "col-span-4 lg:col-span-6")}>
+          <div className="h-full flex flex-col justify-center items-start px-6 lg:p-20 xl:p-32 2xl:p-40">
             <PrismicRichText field={slice.primary.text} />
             {slice.primary.button.url && (
               <Button 
