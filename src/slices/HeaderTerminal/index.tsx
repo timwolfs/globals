c44dbf9394944069c722ac0501df6fb3c70b3680
Tim Wolfs <tim.wolfs@ara.nl>
2023-10-18 15:12:25 +0200

    formatting

--- src/slices/HeaderTerminal/index.tsx
+++ src/slices/HeaderTerminal/index.tsx
@@ -46,16 +46,12 @@ const HeaderTerminal = ({ slice }: HeaderTerminalProps): JSX.Element => {
         <div className="col-span-4 lg:col-span-8 lg:col-start-3 xl:col-span-6 xl:col-start-4">
           <div className="w-full shadow-2xl subpixel-antialiased overflow-hidden rounded-md h-64 bg-black border-black mx-auto">
             <div className="flex items-center h-6 bg-gray-100 border-b border-gray-500 text-center text-black">
-              <div className="flex ml-2 items-center text-center border-red-900 bg-red-500 shadow-inner rounded-full w-3 h-3">
-              </div>
-              <div className="ml-2 border-yellow-900 bg-yellow-500 shadow-inner rounded-full w-3 h-3">
-              </div>
-              <div className="ml-2 border-green-900 bg-green-500 shadow-inner rounded-full w-3 h-3">
-              </div>
+              <div className="flex ml-2 items-center text-center border-red-900 bg-red-500 shadow-inner rounded-full w-3 h-3" />
+              <div className="ml-2 border-yellow-900 bg-yellow-500 shadow-inner rounded-full w-3 h-3" />
+              <div className="ml-2 border-green-900 bg-green-500 shadow-inner rounded-full w-3 h-3" />
               <div className="mx-auto pr-16">
                 <p className="text-center text-sm">tim.wolfs</p>
               </div>
-
             </div>
             <div className="pl-1 pt-1 h-auto  text-green-200 font-mono text-xs bg-black">
               <p className="pb-1">Last login: {formattedDateTime} on ttys073</p>
