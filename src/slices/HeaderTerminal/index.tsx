a253cf04ab284e66a83232145f35b675826dde5b
Tim Wolfs <tim.wolfs@ara.nl>
2023-11-01 10:31:46 +0100

    Reworked terminal header a bit

--- src/slices/HeaderTerminal/index.tsx
+++ src/slices/HeaderTerminal/index.tsx
@@ -42,22 +42,20 @@ const HeaderTerminal = ({ slice }: HeaderTerminalProps): JSX.Element => {
 
   return (
     <section className="container bg-primary py-10 lg:py-32 overflow-hidden">
-      <div className="grid grid-cols-4 lg:grid-cols-12 gap-x-4">
-        <div className="relative col-span-4 lg:col-span-8 lg:col-start-3 xl:col-span-6 xl:col-start-4">
-          <div className="absolute w-full h-full bg-black opacity-20 transform -translate-x-16 translate-y-16 rounded-md" />
-          <div className="relative w-full shadow-2xl subpixel-antialiased overflow-hidden rounded-md h-64 bg-black border-black mx-auto">
-            <div className="flex items-center h-6 bg-gray-100 border-b border-gray-500 text-center text-black">
-              <div className="flex ml-2 items-center text-center border-red-900 bg-red-500 shadow-inner rounded-full w-3 h-3" />
-              <div className="ml-2 border-yellow-900 bg-yellow-500 shadow-inner rounded-full w-3 h-3" />
-              <div className="ml-2 border-green-900 bg-green-500 shadow-inner rounded-full w-3 h-3" />
-              <div className="mx-auto pr-16">
-                <p className="text-center text-sm">tim.wolfs</p>
-              </div>
+      <div className="relative max-w-3xl mx-auto">
+        <div className="absolute w-full h-full bg-black opacity-20 transform -translate-x-16 translate-y-16 rounded-md" />
+        <div className="relative w-full shadow-2xl subpixel-antialiased overflow-hidden rounded-md h-64 bg-black border-black mx-auto">
+          <div className="flex items-center h-6 bg-gray-100 border-b border-gray-500 text-center text-black">
+            <div className="flex ml-2 items-center text-center border-red-900 bg-red-500 shadow-inner rounded-full w-3 h-3" />
+            <div className="ml-2 border-yellow-900 bg-yellow-500 shadow-inner rounded-full w-3 h-3" />
+            <div className="ml-2 border-green-900 bg-green-500 shadow-inner rounded-full w-3 h-3" />
+            <div className="mx-auto pr-16">
+              <p className="text-center text-sm">tim.wolfs</p>
             </div>
-            <div className="pl-1 pt-1 h-auto  text-green-200 font-mono text-xs bg-black">
-              <p className="pb-1">Last login: {formattedDateTime} on ttys073</p>
-              <p className="pb-1">tim.wolfs@MAC-L33T ~ % Hello World! <br />I am a front-end developer located in Rotterdam []</p>
-            </div>
+          </div>
+          <div className="pl-1 pt-1 h-auto  text-green-200 font-mono text-xs bg-black">
+            <p className="pb-1">Current date: {formattedDateTime} on ttys333</p>
+            <p className="pb-1">tim.wolfs@MAC-L33T ~ % Hello World! <br />I am a front-end developer located in Rotterdam []</p>
           </div>
         </div>
       </div>
