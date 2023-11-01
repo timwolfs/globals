b482690322062dc7429d70a623a748ecd9145487
Tim Wolfs <tim.wolfs@ara.nl>
2023-11-01 10:42:21 +0100

    Added custom font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,21 @@
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 import "@/styles/globals.css";
+import { Lato } from "next/font/google";
+
+const lato = Lato({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={lato.className}>
       <head>
         <script async defer src="https://static.cdn.prismic.io/prismic.js?new=true&repo=globals"></script>
       </head>
